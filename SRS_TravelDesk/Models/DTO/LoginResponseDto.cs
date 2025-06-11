d7c476f9289a4859aafa03e019e59ea24e86e7fe
omgupta <omgupta0312@gmail.com>
2025-06-11 22:00:57 +0530

    login controller added

---
+++ SRS_TravelDesk/Models/DTO/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SRS_TravelDesk.Models.DTO
+{
+    public class LoginResponseDto
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
